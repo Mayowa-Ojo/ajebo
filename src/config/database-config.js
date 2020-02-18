c8c76f82b88354cc2da54bef72a7bb9744164d25
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-02-18 13:51:43 -0700

    feat(config): setup mongoose connection

---
+++ src/config/database-config.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+// globals
+const DATABASE_URI = process.env.DATABASE_URI
+
+// setup mongoose connection
+
+mongoose.connect(DATABASE_URI, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+})
+   .then(() => console.log('connected to database'))
+   .catch(err => console.error(err))
