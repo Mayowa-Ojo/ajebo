903ca65e56d0ac65ffba1a1ddf7ff80aca3f9f23
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-02-20 01:59:51 -0700

    feat(module): implement check_diff functionality

--- src/utils/check_diff.js
+++ src/utils/check_diff.js
@@ -1,1 +1,32 @@
 // compare data returned from database and data from scraper function
+const scraper = require('./scraper');
+const sneakerController = require('../database/sneaker_controller');
+
+exports.checkDiff = async function() {
+   
+   try {
+      const liveData = await scraper.scrapeURL();
+      const persistedData = await sneakerController.getSneakers();
+      const hasChanges = []
+      
+      // compare returned data
+      persistedData.map(el_i => {
+         liveData.find(el_j => {
+            // first condition - find matching object
+            // second condition - check for differences in sneaker size
+            if(el_i.productId == el_j.productId && el_i.sizes.length !== el_j.sizes.length) {
+               hasChanges.push({live_data: el_j, stored_data: el_i});
+            }
+         });
+         return;
+      });
+
+      return hasChanges;
+   } catch(err) {
+      console.error(err);
+
+      return;
+   };
+};
+
+module.exports = exports;
