d63cbeb06d9c4792aaed2a9e353cc6775552aeff
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-02-23 19:28:29 -0700

    refactor(server): update server file

--- src/server.js
+++ src/server.js
@@ -7,11 +7,7 @@ const PORT = process.env.PORT;
 const NODE_ENV = process.env.NODE_ENV;
 
 require('./config/database_config');
-// require('./utils/check_diff').checkDiff()
-// require('./utils/scraper').scrapeURL();
-// require('./database/seed');
-// require('./jobs/cron');
-require('./utils/mail');
+require('./jobs/cron');
 
 app.get('/', (req, res) => {
    res.json({
