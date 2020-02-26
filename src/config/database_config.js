9c09a54d1e6ab2ee4a486d54710dac048797a1b2
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-02-26 13:47:17 -0700

    config: add mongo atlas connection string

--- src/config/database_config.js
+++ src/config/database_config.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // globals
-const DATABASE_URI = process.env.DATABASE_URI;
+const { 
+   DATABASE_LOCAL_URI,
+   DATABASE_USER,
+   DATABASE_PASS
+ } = process.env;
+const URI = `mongodb+srv://${DATABASE_USER}:${DATABASE_PASS}@ajebo-0-nuq67.mongodb.net/ajebo?retryWrites=true&w=majority`;
 
 // setup mongoose connection
-
-mongoose.connect(DATABASE_URI, {
+mongoose.connect(URI, {
    useNewUrlParser: true,
    useUnifiedTopology: true
 })
