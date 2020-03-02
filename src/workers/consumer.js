ead381f1b23450ad4a0903392565a91eb8fb5e45
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-03-02 11:50:06 -0700

    feat: setup publish cron message and consume cron message functionality

--- src/workers/consumer.js
+++ src/workers/consumer.js
@@ -18,21 +18,25 @@ connect_rmq(connection => {
       
       channel.consume(queue, (message) => {
          
-         runCron()
-            .then(_ => {
-            console.log(`-- RabbitMQ: process executed in ${queue} - message: ${message.content.toString()}`);
-            channel.ack(message);
-            })
-            .catch(err => console.error(err));
-
-         // require('../utils/scraper').scrapeURL()
-         //    .then(data => {
-         //       console.log(data)
-         //       console.log(`-- RabbitMQ: process executed in ${queue} - message: ${message.content.toString()}`);
-         //       // acknowledge message
-         //       channel.ack(message);
-         //    })
-         //    .catch(err => console.error(err.message));
+         // check type of message sent
+         if(message.content.toString() == 'cron') {
+            runCron()
+               .then(_ => {
+               console.log(`-- RabbitMQ: process executed in ${queue} - message: ${message.content.toString()}`);
+               channel.ack(message);
+               })
+               .catch(err => console.error(err));
+         } else if(message.content.toString() == 'request') {
+            require('../utils/scraper').scrapeURL()
+               .then(data => {
+                  // console.log(data)
+                  console.log(`-- RabbitMQ: process executed in ${queue} - message: ${message.content.toString()}`);
+                  // acknowledge message
+                  channel.ack(message);
+               })
+               .catch(err => console.error(err.message));
+            }
+         
       }, { noAck: false });
       
    });
--- src/workers/publisher.js
+++ src/workers/publisher.js
@@ -1,21 +1,31 @@
 const { connect_rmq, panic } = require('../config/rabbitmq_config');
 
 // connect to RabbitMQ server
-connect_rmq(connection => {
-   
-   // create consumer channel
-   connection.createChannel((err, channel) => {
-      if(err) {
-         return panic(err, connection);
-      }
-      
-      const queue = 'cron';
-      const content = 'get scrape data from url';
-      
-      channel.assertQueue(queue, { durable: true });
-      channel.sendToQueue(queue, new Buffer.from(content), { persistent: true});
-      console.log(`-- RabbitMQ: [x] - message sent to queue: ${queue}`);
+module.exports = (function({ consumerType }) {
 
-      channel.close(() => connection.close());
+   connect_rmq(connection => {
+      
+      // create consumer channel
+      connection.createChannel((err, channel) => {
+         if(err) {
+            return panic(err, connection);
+         }
+         
+         let content;
+
+         if(consumerType == 'cron') {
+            content = 'cron';
+         } else if(consumerType == 'request') {
+            content = 'request';
+         }
+         
+         const queue = 'cron';
+         
+         channel.assertQueue(queue, { durable: true });
+         channel.sendToQueue(queue, new Buffer.from(content), { persistent: true});
+         console.log(`-- RabbitMQ: [x] - message sent to queue: ${queue}`);
+         
+         channel.close(() => connection.close());
+      });
    });
 });
