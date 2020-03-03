64c6fbbbdc7a4945d7407cae2ea6579c8ecd3aa0
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-03-03 08:44:37 -0700

    feat(jobs): implement wake-dyno functionality to prevent dynos from idling

---
+++ src/jobs/wake_dyno.js
@@ -0,0 +1,17 @@
+/**
+ * Heroku dynos go to sleep after 30 mins of inactivity. This obstructs the smooth running of the cron job
+ */
+
+const cron = require('cron');
+const fetch = require('node-fetch');
+
+(() => {
+   const cronJob = new cron.CronJob('0 */25 * * * *', function() {
+      fetch('http://ajebo.herokuapp.com/')
+         .then(res => console.log(`response - ${res.ok}, status - ${res.status}`))
+         .catch(err => console.error(err.message));
+   });
+
+   // start cron-job
+   cronJob.start();
+})();
