1b7c0b694ee9e2aa8d91a9e9cccc4653edeaf239
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-03-05 14:23:26 -0700

    refactor: export connect function

--- src/config/database_config.js
+++ src/config/database_config.js
@@ -11,12 +11,19 @@ const {
    DATABASE_USER,
    DATABASE_PASS
  } = process.env;
-const URI = `mongodb+srv://${DATABASE_USER}:${DATABASE_PASS}@ajebo-0-nuq67.mongodb.net/ajebo?retryWrites=true&w=majority`;
+
+const CLOUD_URI = `mongodb+srv://${DATABASE_USER}:${DATABASE_PASS}@ajebo-0-nuq67.mongodb.net/ajebo?retryWrites=true&w=majority`;
 
 // setup mongoose connection
-mongoose.connect(URI, {
-   useNewUrlParser: true,
-   useUnifiedTopology: true
-})
+exports.connect = function(env) {
+
+   mongoose.connect(env == 'dev' ? DATABASE_LOCAL_URI : CLOUD_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+   })
    .then(() => console.log('-- Database: connected to database'))
    .catch(err => console.error(err));
+
+};
+
+module.exports = exports;
