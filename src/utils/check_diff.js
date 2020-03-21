1b3c6f9b9ccd7d8f16a2116f18e9ccb3845e4946
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-03-20 21:44:39 -0600

    fix: disabled products not appearing in changes

--- src/utils/check_diff.js
+++ src/utils/check_diff.js
@@ -2,20 +2,27 @@
 const scraper = require('./scraper');
 const sneakerController = require('../database/sneaker_controller');
 
-exports.checkDiff = async function() {
-   
+exports.checkDiff = async function(liveData = null, storedData = null) {
+
    try {
-      const liveData = await scraper.scrapeURL();
-      const storedData = await sneakerController.getSneakers();
+      if(!liveData && !storedData) {
+         liveData = await scraper.scrapeURL();
+         storedData = await sneakerController.getSneakers();
+      }
       const hasChanges = [];
-      
+
       // compare returned data
       storedData.map(el_i => {
-         liveData.find(el_j => {
-            if(el_j.productId == el_i.productId && el_j.sizes.length != el_i.sizes.length) {
-               hasChanges.push({storedData: el_i, liveData: el_j});
-            }
-         });
+         const foundData = liveData.find(el_j => el_i.productId == el_j.productId);
+
+         if(foundData == undefined) {
+            hasChanges.push({storedData: el_i, liveData: null});
+            return;
+         }
+
+         if(foundData.productId == el_i.productId && foundData.sizes.length != el_i.sizes.length) {
+            hasChanges.push({storedData: el_i, liveData: foundData});
+         }
          return;
       });
 
