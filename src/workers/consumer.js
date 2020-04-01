b6cd1e1d54429ef2e73c20683ba70837930474a0
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-03-31 23:17:25 -0600

    refactor: spin up multiple consumers to handle requests

--- src/workers/consumer.js
+++ src/workers/consumer.js
@@ -1,44 +1,41 @@
 const { connect_rmq, panic } = require('../config/rabbitmq_config');
 const runCron = require('../jobs/cron');
+const { storeScrapeData } = require("../cache/redis_cache")
 
 // connect to RabbitMQ server
 connect_rmq(connection => {
-   
+
    // spin up multiple consumers
-   // create consumer channel
-   connection.createChannel((err, channel) => {
-      if(err) {
-         return panic(err, connection);
-      }
-      
-      const queue = 'cron';
-      
-      channel.assertQueue(queue, { durable: true });
-      
-      console.log(`-- RabbitMQ: waiting for queue, ${queue}: To exit press CTRL+C`);
-      
-      channel.consume(queue, (message) => {
-         
-         // check type of message sent
-         if(message.content.toString() == 'cron') {
-            runCron()
+   // create 4 consumer channels
+   for(let i = 0; i < 4; i++) {
+
+      connection.createChannel((err, channel) => {
+
+         if(err) {
+            return panic(err, connection);
+         }
+
+         const queue = 'cron';
+
+         channel.assertQueue(queue, { durable: true });
+
+         console.log(`-- RabbitMQ: waiting for queue, ${queue}: To exit press CTRL+C`);
+
+         channel.consume(queue, (message) => {
+
+            // check type of message sent
+            if(message.content.toString() == 'cron') {
+               runCron()
                .then(_ => {
                   console.log(`-- RabbitMQ: process executed in ${queue} - message: ${message.content.toString()}`);
                   channel.ack(message);
                })
                .catch(err => console.error(err));
-         } else if(message.content.toString() == 'request') {
-            require('../utils/scraper').scrapeURL()
-               .then(data => {
-                  // console.log(data)
-                  console.log(`-- RabbitMQ: process executed in ${queue} - message: ${message.content.toString()}`);
-                  // acknowledge message
-                  channel.ack(message);
-               })
-               .catch(err => console.error(err.message));
             }
-         
-      }, { noAck: false });
-      
-   });
+
+         }, { noAck: false });
+
+      });
+
+   }
 });
