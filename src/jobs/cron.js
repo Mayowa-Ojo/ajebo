35945926cc9a8df845274a70aee168a78094e392
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-04-01 14:56:43 -0600

    refactor: strip down cron code for clarity

--- src/jobs/cron.js
+++ src/jobs/cron.js
@@ -1,78 +1,10 @@
 const cron = require('cron');
-const { sendMail } = require('../utils/mail');
-const { checkDiff } = require('../utils/check_diff');
-const { generateHtml } = require('../utils/utils');
 
 // globals
 const cronJob = new cron.CronJob('0 0 */6 * * *', function() {
    // publish task to consumer every <x> hours/mins
-   require('../workers/publisher')({ consumerType: 'cron' });
+   require("../workers/scraper/publisher")({ consumerType: "cron" })
 }, null, true, 'America/Los_Angeles');
 
 cronJob.start();
 
-module.exports = runCron = () => {
-   const rawHtml = (category) => {
-   return `
-   <h3>New Update from Ajebo Tracker 🤖️</h3>
-   *
-   <p>Hi Mayowa, I found some changes in ${category} sizes since last update. Details below</p>
-   *
-   <table style="width:45em; border:1px solid #333;">
-   *
-   <tr style="border:1px solid #333;">
-      <th style="border:1px solid #333; text-align:center">i</th>
-      <th style="border:1px solid #333; text-align:center" colspan="3">storage data</th>
-      <th style="border:1px solid #333; text-align:center" colspan="3">live data</th>
-   </tr>
-   *
-   <tr style="border:1px solid #333;">
-      <td style="border:1px solid #333;"></td>
-      <td style="border:1px solid #333; text-align:center">name</td>
-      <td style="border:1px solid #333; text-align:center">sizes</td>
-      <td style="border:1px solid #333; text-align:center">product-id</td>
-      
-      <td style="border:1px solid #333; text-align:center">name</td>
-      <td style="border:1px solid #333; text-align:center">sizes</td>
-      <td style="border:1px solid #333; text-align:center">product-id</td>
-   </tr>
-   *
-   </table>
-   `;
-   }
-
-   const categories = [
-      "sneakers",
-      "anthem-jackets",
-      "training-kits",
-      "tracksuits"
-   ]
-
-   return Promise.resolve(
-      checkDiff().then(res => {
-         let html = "";
-         // generate dynamic html using tagged templates
-         // temporary change - run loop just once
-         for(let i = 0; i < 1; i++) {
-
-            html += generateHtml`${res[categories[i]]} ${rawHtml(categories[i])}`;
-         }
-         // console.log(html)
-         const message = {
-            from: '"Ajebo Tracker[bot]" <mayowaojo.e@gmail.com>',
-            to: '"Mayowa Ojo" <ojomayowa.e@gmail.com>',
-            subject: 'Notifier: I found changes',
-            text: `Placeholder text - error occured generating html`,
-            html
-         };
-         // console.log(res.length)
-         if(res[categories[0]].length > 0) {
-            // send email if check diff function returns changes
-            sendMail(message);
-            return;
-         } return;
-      })
-      .catch(err => console.error(err))
-   )
-
-};
