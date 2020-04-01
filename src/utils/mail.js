81510c6b9b800c88de8a15cd0f57bc18e53fe39d
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-04-01 15:00:42 -0600

    refactor: handle all mail related code

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,6 +1,7 @@
 // handle email functionality
 const nodemailer = require('nodemailer');
 const config = require('../config/nodemailer_config');
+const { generateHtml } = require('../utils/utils');
 
 // check node env
 if(process.env.NODE_ENV !== 'production') {
@@ -8,10 +9,10 @@ if(process.env.NODE_ENV !== 'production') {
 }
 
 // globals
-const SMTP_HOST=process.env.SMTP_HOST;
-const SMTP_USER=process.env.SMTP_USER;
-const SMTP_PASS=process.env.SMTP_PASS;
-const SMTP_PORT=process.env.SMTP_PORT;
+const SMTP_HOST = process.env.SMTP_HOST;
+const SMTP_USER = process.env.SMTP_USER;
+const SMTP_PASS = process.env.SMTP_PASS;
+const SMTP_PORT = process.env.SMTP_PORT;
 // const SMTP_DEBUG=process.env.SMTP_DEBUG;
 // const SMTP_LOGGER=process.env.SMTP_LOGGER;
 
@@ -35,8 +36,61 @@ transporter.verify((err, success) => {
    console.log('-- Nodemailer: listening for for messages...');
 });
 
-exports.sendMail = function({from, to, subject, text, html}) {
-   const message = {from, to, subject, text, html}
+exports.sendMail = function(data) {
+   const rawHtml = (category) => {
+      return `
+      <h3>New Update from Ajebo Tracker 🤖️</h3>
+      *
+      <p>Hi Mayowa, I found some changes in ${category} sizes since last update. Details below</p>
+      *
+      <table style="width:45em; border:1px solid #333;">
+      *
+      <tr style="border:1px solid #333;">
+         <th style="border:1px solid #333; text-align:center">i</th>
+         <th style="border:1px solid #333; text-align:center" colspan="3">storage data</th>
+         <th style="border:1px solid #333; text-align:center" colspan="3">live data</th>
+      </tr>
+      *
+      <tr style="border:1px solid #333;">
+         <td style="border:1px solid #333;"></td>
+         <td style="border:1px solid #333; text-align:center">name</td>
+         <td style="border:1px solid #333; text-align:center">sizes</td>
+         <td style="border:1px solid #333; text-align:center">product-id</td>
+         
+         <td style="border:1px solid #333; text-align:center">name</td>
+         <td style="border:1px solid #333; text-align:center">sizes</td>
+         <td style="border:1px solid #333; text-align:center">product-id</td>
+      </tr>
+      *
+      </table>
+      `;
+   };
+
+   const categories = [
+      "sneakers",
+      "anthem-jackets",
+      "training-kits",
+      "tracksuits"
+   ];
+
+   let html = "";
+
+   // build html
+   for(let i = 0; i < categories.length; i++) {
+
+      html += generateHtml`${data[categories[i]]} ${rawHtml(categories[i])}`;
+
+   }
+
+   const message = {
+      from: '"Ajebo Tracker[bot]" <mayowaojo.e@gmail.com>',
+      to: '"Mayowa Ojo" <ojomayowa.e@gmail.com>',
+      subject: 'Notifier: I found changes',
+      text: `Placeholder text - error occured generating html`,
+      html
+   };
+
+   // send email if check diff function returns changes
    transporter.sendMail(message, (err, info) => {
       if(err) {
          console.log(`Oops! something went wrong: ${err.message}`);
@@ -45,7 +99,7 @@ exports.sendMail = function({from, to, subject, text, html}) {
       
       console.log("Message sent %s: ", info.messageId);
       console.log("Preview URL %s: ", nodemailer.getTestMessageUrl(info));
-
+      
       return;
    });
 }
