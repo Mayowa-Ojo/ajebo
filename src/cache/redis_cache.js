9c7662f33a263c08c26745c3d83d698f13b68813
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-03-31 23:17:43 -0600

    feat: setup redis cache

---
+++ src/cache/redis_cache.js
@@ -0,0 +1,22 @@
+// redis cache - temporarily store scraped data from consumers
+const client =  require('../config/redis_config');
+
+exports.storeScrapeData = function() {
+
+}
+
+exports.initCounter = function() {
+
+}
+
+exports.incrementCounter = function() {
+
+}
+
+exports.checkCounter  = function() {
+
+}
+
+exports.cleanUp = function() {
+
+}
