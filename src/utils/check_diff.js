3261a103419cf8822e2461174cf5a8b7d5db46bf
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-04-01 14:59:54 -0600

    refactor: checkDiff should receive data for one product category

--- src/utils/check_diff.js
+++ src/utils/check_diff.js
@@ -5,61 +5,52 @@ const { getAnthemJackets } = require('../database/anthem_jackets/controller');
 const { getTrainingKits } = require('../database/training_kits/controller');
 const { getTrackSuits } = require('../database/tracksuits/controller');
 
-exports.checkDiff = async function(liveData = null, storedData = null) {
+exports.checkDiff = async function(category, liveData = null, storedData = null) {
 
-   const diff = {};
+   // const diff = {};
 
-   const categories = [
-      "sneakers",
-      "anthem-jackets",
-      "training-kits",
-      "tracksuits"
-   ];
-
-   const controllers = [
-      getSneakers,
-      getAnthemJackets,
-      getTrainingKits,
-      getTrackSuits
-   ];
+   const controllers = {
+      "sneakers": getSneakers,
+      "anthem-jackets": getAnthemJackets,
+      "training-kits": getTrainingKits,
+      "tracksuits": getTrackSuits
+   };
    // temporary - run only one loop for sneakers.
-   for(let i = 0; i < 1; i++) {
+   // for(let i = 0; i < 1; i++) {
 
-      try {
-         if(!liveData && !storedData) {
-            liveData = await scraper.scrapeURL(categories[i]);
-            storedData = await controllers[i]();
+   try {
+      if(!liveData && !storedData) {
+         liveData = await scraper.scrapeURL(category);
+         storedData = await controllers[category]();
+      }
+      const hasChanges = [];
+
+      // compare returned data
+      for(let i = 0; i < storedData.length; i++) {
+         const foundData = liveData.find(el_j => storedData[i].productId == el_j.productId);
+
+         if(foundData == undefined) {
+            hasChanges.push({storedData: storedData[i], liveData: null});
+            continue;
          }
-         const hasChanges = [];
 
-         // compare returned data
-         for(let i = 0; i < storedData.length; i++) {
-            const foundData = liveData.find(el_j => storedData[i].productId == el_j.productId);
+         if(foundData.sizes.length != storedData[i].sizes.length) {
+            hasChanges.push({storedData: storedData[i], liveData: foundData});
+            continue;
+         }
 
-            if(foundData == undefined) {
-               hasChanges.push({storedData: storedData[i], liveData: null});
-               continue;
-            }
-
-            if(foundData.sizes.length != storedData[i].sizes.length) {
-               hasChanges.push({storedData: storedData[i], liveData: foundData});
-               continue;
-            }
-
-         };
-
-        diff[categories[i]] = hasChanges;
-        liveData = null;
-        storedData = null;
-      } catch(err) {
-         console.error(err);
-
-         break;
       };
 
-   }
+      return hasChanges;
+   } catch(err) {
+      console.error(err);
 
-   return diff;
+      return;
+   };
+
+   // }
+
+   // return diff;
 };
 
 module.exports = exports;
