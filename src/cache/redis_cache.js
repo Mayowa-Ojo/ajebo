eb8d414090cea976aeff0e131cd6f6797629be71
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-04-03 14:46:26 -0600

    refactor: wrape redis methods with native promisify

--- src/cache/redis_cache.js
+++ src/cache/redis_cache.js
@@ -1,24 +1,31 @@
 // redis cache - temporarily store scraped data from consumers
+const util = require("util")
 const client =  require('../config/redis_config');
+// promisify methods
+const asyncGet = util.promisify(client.get).bind(client);
+const asyncSet = util.promisify(client.set).bind(client);
+const asyncDel = util.promisify(client.del).bind(client);
+const asyncIncrby = util.promisify(client.incrby).bind(client);
+
 
 /**
  * store data in hash field
  */
-exports.storeScrapeData = function(data, key) {
-   client.set(key, JSON.stringify(data), (err, reply) => {
-      if(err) {
-         console.error(`Error: ${err}`);
-         return;
-      }
-      
-      // increment counter
-      incrementCounter();
-      console.log(`[x] --redis: @storeScrapeData - ${reply}`);
+const storeScrapeData = async function(data, key) {
 
-   });
+   try {
+      const reply = await asyncSet(key, JSON.stringify(data));
+
+      // increment counter
+      await incrementCounter();
+      console.log(`[x] --redis: @storeScrapeData - ${reply}`);
+   } catch(err) {
+      console.error(err);
+      return;
+   }
 }
 
-exports.fetchScrapeData = function() {
+const fetchScrapeData = async function() {
    const keys = [
       "sneakers",
       "anthem-jackets",
@@ -31,51 +38,77 @@ exports.fetchScrapeData = function() {
    // fetch data for all keys and aggregate
    for(let i = 0; i < keys.length; i++) {
 
-      client.get(keys[i], (err, reply) => {
-         if(err) {
-            console.error(`Error: ${err}`);
-            return;
-         }
+      try {
+         const reply = await asyncGet(keys[i]);
 
          data[keys[i]] = JSON.parse(reply);
-      })
+      } catch(err) {
+         console.error(err);
+         return;
+      }
    }
 
    return data;
 }
 
+const initializeCounter = async function() {
+
+   try {
+      const reply = await asyncSet("counter", 0);
+      
+      console.log(`@initializeCounter: ${reply}`);
+   } catch(err) {
+      console.error(err);
+      return;
+   }
+}
+
+const fetchCounter = async function() {
+
+   try {
+      const reply = await asyncGet("counter");
+
+      return JSON.parse(reply);
+   } catch(err) {
+      console.error(err);
+      return;
+   }
+}
+
 /**
  * increments value at hash field. Initializes of doesn't exist
  */
-exports.incrementCounter = function() {
-   client.hincrby("counter", "count", 1, (err, reply) => {
-      if(err) {
-         console.error(`Error: ${err}`);
-         return;
-      }
+const incrementCounter = async function() {
+
+   try {
+      const reply = await asyncIncrby("counter", 1);
 
       console.log(`[x] --redis: @incrementCounter - ${reply}`);
-   });
+   } catch(err) {
+      console.error(err);
+      return;
+   }
 }
 
 /**
  * checks the current counter state
  */
-exports.checkCounter  = function() {
-   return client.hget("counter", "count", (err, reply) => {
-      if(err) {
-         console.error(`Error: ${err}`);
-         return;
-      }
+const checkCounter = async function(responseCallback) {
+
+   try {
+      const reply = await asyncGet("counter");
 
       return JSON.parse(reply) >= 3;
-   });
+   } catch(err) {
+      console.error(err);
+      return;
+   }
 }
 
 /**
  * cleanup cache after all consumers have executed
  */
-exports.cleanUp = function() {
+const cleanUp = async function() {
    const keys = [
       "sneakers",
       "anthem-jackets",
@@ -85,22 +118,33 @@ exports.cleanUp = function() {
 
    for(let i = 0; i < keys.length; i++) {
 
-      client.del(keys[i], (err, reply) => {
-         if(err) {
-            console.error(`Error: ${err}`);
-            return;
-         }
-         
-         console.log(`[x] --redis: @cleanUp - ${reply}`);
-      });
-   }
+      try {
+         const reply = await asyncDel(keys[i]);
 
-   client.hdel("counter", "count", (err, reply) => {
-      if(err) {
-         console.error(`Error: ${err}`);
+         console.log(`[x] --redis: @cleanUp - ${reply}`);
+      } catch(err) {
+         console.log(err);
          return;
       }
+   }
 
-      console.log(`[x] --redis: @cleanUp - ${reply}`);
-   });
+      // reset counter
+   try {
+      const reply = await asyncSet("counter", 0);
+
+      console.log(`[x] --redis @cleanUp [reset counter]: ${reply}`);
+   } catch(err) {
+      console.log(err);
+      return;
+   }
 }
+
+exports.incrementCounter = incrementCounter;
+exports.storeScrapeData = storeScrapeData;
+exports.fetchScrapeData = fetchScrapeData;
+exports.cleanUp = cleanUp;
+exports.checkCounter = checkCounter;
+exports.fetchCounter = fetchCounter;
+exports.initializeCounter = initializeCounter;
+
+module.exports = exports;
